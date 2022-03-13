4eaee8f20285001ba7b7c87c1c0c3a8927955e68
Mohamed Ali <im_bousba@esi.dz>
2022-03-13 17:37:13 +0100

    add convert function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createWorker } from "tesseract.js";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState();
+  const [generatedText, setGeneratedText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const select = e => {
+    setImage(e.target.files[0]);
+    setGeneratedText("");
+  };
+
+  const convert = async () => {
+    setLoading(true);
+    const worker = createWorker();
+
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text }
+    } = await worker.recognize(image);
+    setGeneratedText(text);
+    setLoading(false);
+    await worker.terminate();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Image to Text</h1>
+      <input type="file" accept="image/*" onChange={select} />
+
+      <div className="d-flex justify-content-between mt-4">
+        <div>
+          {image && (
+            <img
+              src={URL.createObjectURL(image)}
+              alt=""
+              style={{ height: 400, width: 400 }}
+            />
+          )}
+        </div>
+        <div className="mx-3">
+          {image && (
+            <button className="btn btn-primary" onClick={convert}>
+              {loading ? "Converting..." : "Convert"}
+            </button>
+          )}
+        </div>
+        <div>
+          {generatedText && image && (
+            <p style={{ padding: 10, border: "1px solid #0000ff" }}>
+              {generatedText}
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
